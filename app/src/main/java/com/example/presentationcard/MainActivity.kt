3faeaef9f7390f566b49e79c734ea491f9beca8e
CarlosMontoyaHidalgo <carlosmh561@gmail.com>
2023-10-13 12:34:09 +0100

    Cambiando las strings

--- app/src/main/java/com/example/presentationcard/MainActivity.kt
+++ app/src/main/java/com/example/presentationcard/MainActivity.kt
@@ -9,19 +9,12 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Email
-import androidx.compose.material.icons.filled.Phone
-import androidx.compose.material.icons.filled.Place
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -31,17 +24,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.presentationcard.ui.theme.PresentationCardTheme
-import java.time.format.TextStyle
 
 
 class MainActivity : ComponentActivity() {
@@ -91,8 +78,6 @@ fun CardPamn(){
         Text(text = "texto complementario", color = Color(0xFF3ddc84))
     }
 
-    ////////////////////////////////////////////////////
-
     Column(
         modifier = Modifier
             .fillMaxWidth()
@@ -101,9 +86,9 @@ fun CardPamn(){
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
     ){
-        CustomRow(iconId = R.drawable.icon_phone, text = "123-456-789", modifier = Modifier)
-        CustomRow(iconId = R.drawable.icon_email, text = "email@gmail.com", modifier = Modifier)
-        CustomRow(iconId = R.drawable.icon_share, text = "Comparte", modifier = Modifier)
+        CustomRow(iconId = R.drawable.icon_phone, text = stringResource(R.string.telephone_number), modifier = Modifier)
+        CustomRow(iconId = R.drawable.icon_email, text = stringResource(R.string.email), modifier = Modifier)
+        CustomRow(iconId = R.drawable.icon_share, text = stringResource(R.string.share_information), modifier = Modifier)
     }
 }
 
@@ -132,7 +117,6 @@ fun CustomRow(iconId: Int, text: String, modifier: Modifier){
 @Composable
 fun GreetingPreview() {
     PresentationCardTheme {
-        //MyCard()
         CardPamn()
     }
 }
